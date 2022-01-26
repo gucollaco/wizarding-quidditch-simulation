902a1d0978babe6bf4cd87972ac7c7cc405f0658
Gustavo Collaco <gustavo.m.collaco@gmail.com>
2022-01-26 15:14:49 -0300

    feat: initial camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private float moveSpeed = 0.2f;
+    private float moveSpeed = 0.1f;
+    private float rotateSpeed = 2f;
 
-    private void Update () {
+    private void LateUpdate () {
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
             transform.position -= moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         }
         if (Input.GetKey(KeyCode.Q))
         {
-            transform.position += moveSpeed * new Vector3(0, 1, 0);
+            transform.position += moveSpeed * transform.up;
         }
         if (Input.GetKey(KeyCode.E))
         {
-            transform.position += moveSpeed * new Vector3(0, -1, 0);
+            transform.position -= moveSpeed * transform.up;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            transform.eulerAngles += rotateSpeed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
         }
     }
 }
